5469b723667cce95f145fb479f54e6ad062f0061
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-22 20:08:27 +0600

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return (
+      <div>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <Link to="/">Weather App</Link>
+          </div>
+          <div className="flex-none">
+            <button>
+              <input type="checkbox" className="toggle" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export default NavBar;
