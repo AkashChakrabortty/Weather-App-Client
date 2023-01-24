9c5a6c0ab3f017d74d260d4b4055badf1baaf79e
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-25 01:20:57 +0600

    setinterval

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 const Card = ({city}) => {
  const position = [city.lat,city.lon];
-// const position = [22.3542, 90.3181];
  const [weather,setWeather] = useState({});
+ const [reFetch,setRefetch] = useState(false);
  useEffect(() => {
    fetch(
      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=dfddcdf016eccd19fad36794d34d98db`
    )
      .then((res) => res.json())
      .then((data) => {
-      const weatherInfo = {
-        description: data.weather[0].description,
-        main: data.weather[0].main,
-        windSpeed: data.wind.speed,
-      };
-      setWeather(weatherInfo)
+       const weatherInfo = {
+         description: data.weather[0].description,
+         main: data.weather[0].main,
+         windSpeed: data.wind.speed,
+       };
+       setWeather(weatherInfo);
      });
- }, []);
-const HandleWeather = () => {
- 
-}
- 
+ }, [reFetch]);
+ //for refresh after 5min
+ setInterval(() => {
+   setRefetch(!reFetch);
+ }, 300000);
     return (
       <div>
         <div className="card w-4/5 mx-auto bg-base-100 shadow-xl image-full">
@@ -32,14 +32,8 @@ const HandleWeather = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-
               <Marker
                 position={position}
-                eventHandlers={{
-                  click: (e) => {
-                    HandleWeather();
-                  },
-                }}
               >
                 <Popup>
                   <p>Main: {weather.main}</p>
