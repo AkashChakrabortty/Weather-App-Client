3b03515f2c48ca87ce6717790d4b540f0ee7c6e8
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-25 01:05:49 +0600

    show weather info

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,22 +9,16 @@ const Home = () => {
       .then((data) => setCities(data));
   },[])
    
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.openweathermap.org/data/2.5/weather?q=Tongi,1710,+880&appid=dfddcdf016eccd19fad36794d34d98db`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // }, []);
+
     return (
-      <div>
-        {
-        cities.map(city => {return (
-          <>
-            <Card city={city} key={city._id}></Card>
-          </>
-        );})
-        }
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-4">
+        {cities.map((city) => {
+          return (
+            <div key={city._id}>
+              <Card city={city} key={city._id}></Card>
+            </div>
+          );
+        })}
       </div>
     );
 };
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 const Card = ({city}) => {
-  // console.log(city)
  const position = [city.lat,city.lon];
 // const position = [22.3542, 90.3181];
-
+ const [weather,setWeather] = useState({});
+ useEffect(() => {
+   fetch(
+     `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=dfddcdf016eccd19fad36794d34d98db`
+   )
+     .then((res) => res.json())
+     .then((data) => {
+      const weatherInfo = {
+        description: data.weather[0].description,
+        main: data.weather[0].main,
+        windSpeed: data.wind.speed,
+      };
+      setWeather(weatherInfo)
+     });
+ }, []);
+const HandleWeather = () => {
+ 
+}
+ 
     return (
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
-          <h3>{city.CityName}</h3>
+        <div className="card w-4/5 mx-auto bg-base-100 shadow-xl image-full">
           <div className="card-body">
+            <h3 className="text-2xl text-center">{city.CityName}</h3>
             <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={position}>
+
+              <Marker
+                position={position}
+                eventHandlers={{
+                  click: (e) => {
+                    HandleWeather();
+                  },
+                }}
+              >
                 <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
+                  <p>Main: {weather.main}</p>
+                  <p>Description: {weather.description}</p>
+                  <p>Wind Speed: {weather.windSpeed}</p>
                 </Popup>
               </Marker>
             </MapContainer>
